37511bb4d7a3d0a7edd70f682a399500e09038e9
Ahmad Riswanto <ahmadriswanto89@gmail.com>
2023-08-10 23:43:39 +0700

    add connect walllet

--- src/components/MintCard/MintCard.tsx
+++ src/components/MintCard/MintCard.tsx
@@ -1,7 +1,56 @@
+import { switchNetwork } from "@wagmi/core";
+import { Web3Button, useWeb3Modal } from "@web3modal/react";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useAccount, useContractWrite, useNetwork, usePrepareContractWrite, useWaitForTransaction } from "wagmi";
+import { base } from 'wagmi/chains'
 
 const MintCard = () => {
+
+  const [connected, setConnected] = useState(false);
+  const [chainId, setChainId] = useState(-1);
+
+  const { open, close } = useWeb3Modal();
+  const { chain } = useNetwork();
+  const { isConnected } = useAccount();
+
+  useEffect(() => {
+    setConnected(isConnected);
+    setChainId(chain!.id);
+  }, [chain, isConnected])
+
+  // const { config } = usePrepareContractWrite({
+  //   address: '0xFBA3912Ca04dd458c843e2EE08967fC04f3579c2',
+  //   abi: [
+  //     {
+  //       name: 'mint',
+  //       type: 'function',
+  //       stateMutability: 'nonpayable',
+  //       inputs: [],
+  //       outputs: [],
+  //     },
+  //   ],
+  //   functionName: 'mint',
+  // })
+
+  // const { data, write } = useContractWrite(config)
+ 
+  // const { isLoading, isSuccess } = useWaitForTransaction({
+  //   hash: data?.hash,
+  // })
+
+  const onAction = () => {
+    if(isConnected){
+      if(chain?.id === base.id){
+
+      } else {
+        switchNetwork({ chainId: base.id});
+      }
+    } else {
+      open();
+    }
+  }
+
   return (
     <div className="min-w-[350px] min-h-full rounded-xl bg-primary border-black flex flex-col border border-tertiary shadow-lg">
       {/* header */}
@@ -28,10 +77,24 @@ const MintCard = () => {
         </div>
       </div>
       <div className="w-full px-5 pb-5">
-        <button className="w-full h-[40px]  rounded-xl bg-black font-primaryBold text-primary hover:scale-105">
-          <p className=" text-white uppercas  font-semibold font-mono">
-            MINTING
-          </p>
+        <button onClick={onAction} className="w-full h-[40px]  rounded-xl bg-black font-primaryBold text-primary hover:scale-105">
+            {
+              connected ? <>
+              {
+                chainId === base.id ?  
+                <p className=" text-white uppercas  font-semibold font-mono">
+                  MINTING
+                </p> : 
+                <p className=" text-white uppercas  font-semibold font-mono">
+                  SWITCH NETWORK
+                </p>
+              }
+              </> :
+              <p className=" text-white uppercas  font-semibold font-mono">
+                CONNECT WALLET
+              </p>
+            }
+            <></>
         </button>
       </div>
     </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,11 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Loading from "@/components/Loader";
 
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { Web3Modal } from '@web3modal/react'
+import { configureChains, createConfig, WagmiConfig } from 'wagmi'
+import { base } from 'wagmi/chains'
+
 export default function App({ Component, pageProps }: AppProps) {
   const [loading, setLoading] = React.useState(true);
 
@@ -14,9 +19,23 @@ export default function App({ Component, pageProps }: AppProps) {
     return () => clearTimeout(timeout); // Clear the timeout when the component is unmounted
   }, []);
 
+  const chains = [base]
+  const projectId = '6a695714116d7d29cab5eaedecb82460'
+
+  const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
+  const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors: w3mConnectors({ projectId, chains }),
+    publicClient
+  })
+  const ethereumClient = new EthereumClient(wagmiConfig, chains)
+
   return (
     <>
+    <WagmiConfig config={wagmiConfig}>
       {loading ? <Loading /> : null} <Component {...pageProps} />{" "}
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    </WagmiConfig>
     </>
   );
 }
